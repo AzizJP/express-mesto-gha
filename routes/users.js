435e63df750878d12e3a593a2b8ea7711a19fdb9
Aziz <t1-zer@yandex.ru>
2022-10-11 11:23:28 +0300

    feat: add new router

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,13 @@ const {
   getUserById,
   updateUser,
   updateAvatarById,
+  getUserMe,
 } = require('../controllers/users');
 const validateAvatarUpdate = require('../middlewares/validateAvatarUpdate');
 const validateUserUpdate = require('../middlewares/validateUserUpdate');
 
 router.get('/users', getUsers);
+router.get('/users/me', getUserMe);
 router.get('/users/:userId', getUserById);
 router.patch('/users/me', validateUserUpdate, updateUser);
 router.patch('/users/me/avatar', validateAvatarUpdate, updateAvatarById);
